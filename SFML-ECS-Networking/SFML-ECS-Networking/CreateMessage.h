347059e679a7d905b0b8fffb62d4c913d6c1830d
Thomas <2201871@uad.ac.uk>
2026-05-28 14:02:09 +0100

    removed uneeded files

--- SFML-ECS-Networking/SFML-ECS-Networking/CreateMessage.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct CreateMessage
-{
-	int packetHeader = 1;
-	int id = 0;
-	sf::Vector2<float> initialPos = { 100, 100 };
-};
--- SFML-ECS-Networking/SFML-ECS-Networking/DeleteMessage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct DeleteMessage
-{
-	int header = 3;
-	int id = 0;
-};
